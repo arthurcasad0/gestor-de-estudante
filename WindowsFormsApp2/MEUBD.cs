9b3b77d9dfa31232db33bf22f4de2d7b968222e1
arthurcasad0 <arthurcasadoacj@gmail.com>
2023-06-06 17:44:40 -0300

    Update MEUBD.cs
    programação do guit hub dentro do visual studio my skl the by time privait woid txt usuario .tg

--- WindowsFormsApp2/MEUBD.cs
+++ WindowsFormsApp2/MEUBD.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using my sql data .my sql 
+using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApp2
 {
     internal class MEUBD
     {
+        private MySqlConnection conexao = new MySqlConnection("datasource=localhost;username=root;password=;database=t5_sga_db");
+
+        public MySqlConnection getConexao
+        {
+            get { return conexao; } 
+        }
+
+        //Método para ABRIR/INICIALIZAR an conexão
+        public void abrirConexao()
+        {
+            if (conexao.State == ConnectionState.Closed)
+            {
+                conexao.Open();
+            }
+        }
+
+
+
+        //Método para FECHAR/ENCERAR an conexão
+        public void fecharConexao()
+        {
+            if (conexao.State == ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }MEUBD banco de dados 
     }
 }
+   
